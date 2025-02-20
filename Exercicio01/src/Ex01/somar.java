bd8fd3f5266b740d4e5b5be0c4c5060e7f04fa96
Daniel Marques <130517265+JackDanielxs@users.noreply.github.com>
2025-02-19 23:55:09 -0300

    Exercicio 01
    1. Criar um novo projeto Java.
    2. Criar uma nova classe.
    3. Implementar, compilar e executar um programa para somar dois números inteiros

---
+++ Exercicio01/src/Ex01/somar.java
@@ -0,0 +1,15 @@
+package Ex01;
+import java.util.*;
+public class somar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int soma = n1 + n2;
+		System.out.println("Soma: " + soma);
+		sc.close();
+	}
+
+}
