db47000d4eee4658a9268ca851212f80fc17f47e
Daniel Marques <130517265+JackDanielxs@users.noreply.github.com>
2025-02-20 22:55:24 -0300

    Add files via upload

--- Exercicio01/src/Ex01/somar.java
+++ Exercicio01/src/Ex01/somar.java
@@ -1,9 +1,9 @@
 package Ex01;
 import java.util.*;
-public class somar {
+class somar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Scanner sc = new Scanner(System.in);
 		int n1 = sc.nextInt();
 		int n2 = sc.nextInt();
